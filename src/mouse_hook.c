f8395b38629024d1f74fc41a0eae5b55f3f5038b
Pierre RICHARD DE SOULTRAIT <prichard@student.42.fr>
2016-06-09 18:00:03 +0200

    DONE

--- src/mouse_hook.c
+++ src/mouse_hook.c
@@ -6,7 +6,7 @@
 /*   By: prichard <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/06/06 18:16:12 by prichard          #+#    #+#             */
-/*   Updated: 2016/06/09 15:44:59 by prichard         ###   ########.fr       */
+/*   Updated: 2016/06/09 17:59:51 by prichard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,6 @@ void	zoom(int key, int x, int y, t_all *all)
 	ALL->zoom_y = (y * (ALL->y2 - ALL->y1) / (double)all->mlx->width + ALL->y1);
 	if (key == 5)
 	{
-
 		ALL->x1 = (double)ALL->zoom_x + (ALL->x1 - ALL->zoom_x) * 1.1;
 		ALL->x2 = (double)ALL->zoom_x + (ALL->x2 - ALL->zoom_x) * 1.1;
 		ALL->y1 = (double)ALL->zoom_y + (ALL->y1 - ALL->zoom_y) * 1.1;
